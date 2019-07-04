b3b7cd832fcf57673347ac36004a6ad957b2f39d
tsubotaL <kt.tsubo@gmail.com>
2019-07-04 15:03:55 +0900

    code for uncond

--- easy_gold/gen_images.py
+++ easy_gold/gen_images.py
@@ -3,11 +3,12 @@ import shutil
 import numpy as np
 import argparse
 import chainer
+import chainercv
 from PIL import Image
 
 base = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(base, '../'))
-from evaluation import gen_images_with_condition
+from evaluation import gen_images
 import yaml
 import yaml_utils
 
@@ -24,9 +25,7 @@ def main():
     parser.add_argument('--gpu', '-g', type=int, default=0)
     parser.add_argument('--results_dir', type=str, default='./results/gans')
     parser.add_argument('--snapshot', type=str, default='')
-    parser.add_argument('--rows', type=int, default=100)
-    parser.add_argument('--columns', type=int, default=100)
-    parser.add_argument('--classes', type=int, nargs="*", default=None)
+    parser.add_argument('--n_samples', type=int, default=10000)
     args = parser.parse_args()
     chainer.cuda.get_device_from_id(args.gpu).use()
     config = yaml_utils.Config(yaml.load(open(args.config_path)))
@@ -34,21 +33,22 @@ def main():
     gen = load_models(config)
     gen.to_gpu(args.gpu)
     out = args.results_dir
+    if not os.path.exists(out):
+        os.makedirs(out)
+
     chainer.serializers.load_npz(args.snapshot, gen)
     np.random.seed(1234)
-    classes = tuple(args.classes) if args.classes is not None else np.arange(0, gen.n_classes, dtype=np.int32)
-    for c in classes:
-        with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
-            x = gen_images_with_condition(gen, c=c, n=args.rows * args.columns, batchsize=args.rows * args.columns)
-        _, _, h, w = x.shape
-        x = x.reshape((args.rows, args.columns, 3, h, w))
-        x = x.transpose(0, 3, 1, 4, 2)
-        x = x.reshape((args.rows * h, args.columns * w, 3))
+    with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
+        x = gen_images(gen, n=args.n_samples, batchsize=args.n_samples)
 
-        save_path = os.path.join(out, '{}.png'.format(str(c)))
-        if not os.path.exists(out):
-            os.makedirs(out)
-        Image.fromarray(x).save(save_path)
+    n, c, h, w = x.shape
+    for i, img in enumerate(x):
+        chainercv.utils.write_image(img, os.path.join(out, '{:05d}.png'.format(i)))
+    rows, columns = args.n_samples, args.n_samples // 100
+    x = x.reshape((rows, columns, 3, h, w))
+    x = x.transpose(0, 3, 1, 4, 2)
+    x_all = x.reshape((rows * h, columns * w, 3))
+    chainercv.utils.write_image(x_all, os.path.join(out, 'all.png'))
 
 
 if __name__ == '__main__':
